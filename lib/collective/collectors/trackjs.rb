178d979c8504e254d14af29a07efaec867e3287c
Sophie Dawson <sophie@remind101.com>
2016-04-27 12:48:00 -0700

    Working version of trackjs collector with params

--- lib/collective/collectors/trackjs.rb
+++ lib/collective/collectors/trackjs.rb
@@ -8,49 +8,107 @@ module Collective::Collectors
 
     resolution '60s'
 
-    # def initialize(mostRecentId = nil, options = {})
-    #   @mostRecentId = mostRecentId
-    #   @options = options
-    # end
+    def initialize(lastSeenId = nil, options)
+      @lastSeenId = lastSeenId
+      super(options)
+    end
 
     collect do
+      p "COLLECT@@@@@@@@@@@@@@@@@@@@@@@"
       instrument_errors
     end
 
     private
 
     def instrument_errors
-      paged("/#{customer_id}/v1/errors").each do |resp|
-        p "resp"
-        pp resp
-        # p "most recent id"
-        # pp mostRecentId
-        errors = resp.body['data']
-
-        errors.each do |error|
-          instrument 'trackjs.url.errors', error['count'], source: error['key'], type: 'count'
-        end
+      paged("/#{customer_id}/v1/errors").each do |error|
+        p "tracking an error"
+        p "rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+        # p error
+        # errors.each do |error|
+        #   instrument 'trackjs.url.errors', error['count'], source: error['key'], type: 'count'
+        # end
       end
     end
 
-    # TODO: test paging
     def paged(path, params={})
       Enumerator.new do |yielder|
         page = 1
-        pageSize = 500
+        pageSize = 10
+        maxPages = 3
         resp = get_page(path, params, page, pageSize)
+        currentStartId = nil # TODO: better variable name
+        getAnotherPage = true
 
-        # TODO: test response handling
-        while page < 2 do # resp.body['metadata']['hasMore'] == true do
-          yielder.yield resp.body['data'] # pull out each error automatically?
-          # @mostRecentId = page
-          page += 1
-          resp = get_page(path, params, page)
+        # p "response data"
+        # p resp.body['data']
+        p "response metadata"
+        p resp.body['metadata']
+        p "@lastSeenId"
+        p @lastSeenId
+        p "************************************************"
+
+        data = resp.body['data']
+        # TODO: add documentation about what we're doing to handle the fact
+        # that trackjs only lets you get data at a minimum granularity of 1
+        # day.
+        if data.length
+          # p "data[0]"
+          # p data[0]
+          # p "data[0].id"
+          # p data[0]['id']
+          currentStartId = data[0]['id']
+          p "storing most recent Id:"
+          p currentStartId
+          p "************************************************"
+
+          # This will loop over every single event ever the first time it's run
+          # if it's not constrained somehow
+          while resp.body['metadata']['hasMore'] == true and page <= maxPages do # page < 3 do # resp.body['metadata']['hasMore'] == true do
+            resp.body['data'].each do |error|
+              p "error"
+              p error['message']
+              p error['id']
+              if error['id'] == @lastSeenId
+                p "BREAKING OUT OF LOOP. I'VE SEEN THIS ERROR BEFORE"
+                getAnotherPage = false
+                break
+              else
+                yielder.yield error
+              end
+            end
+
+            if getAnotherPage
+              p ">>>going to get another page"
+              page += 1
+              resp = get_page(path, params, page, pageSize)
+            else
+              break
+            end
+          end
+          # check why i broke out of the while loop
+          # options:
+          # hit something i'd seen before
+          # hit the very end of the errors list
+          # hit the max page limit
+          if getAnotherPage == false
+            p "1111111 Finished while loop because I saw an error i'd seen before"
+          elsif page > maxPages
+            p "2222222 Finished while loop because I hit the max page limit"
+          else
+            p "3333333 Finished while loop because I hit the end of the errors list"
+          end
+          p "setting last seen id to"
+          p currentStartId
+          @lastSeenId = currentStartId
         end
       end
     end
 
-    def get_page(path, params, page, pageSize)
+    def get_page(path, params, page, pageSize = 500)
+      p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>>>>>>>>>>>>"
+      p "getting a new page of results"
+      p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>>>>>>>>>>>>"
       client.get(path, params.merge(page: page, size: pageSize)) do |req|
         req.headers['Authorization'] = api_key
       end
