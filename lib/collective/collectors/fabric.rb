0d193601bce7b1df6c157e9f5f6f883c1c4a89e7
Alex Leffelman <alex@remind101.com>
2017-04-04 13:20:08 -0500

    Updated resolution, fixed var name;

--- lib/collective/collectors/fabric.rb
+++ lib/collective/collectors/fabric.rb
@@ -2,7 +2,7 @@ module Collective::Collectors
   class Fabric < Collective::Collector
     requires :fabricio
 
-    resolution '24h'
+    resolution '86400s'
 
     collect do
       group 'fabric' do |group|
@@ -21,7 +21,7 @@ module Collective::Collectors
       now = Date.today
       seven_days_ago = now - 7
 
-      fabric.app.crashfree('53b2fb0fe499b52221000147', seven_days_ago.to_time.to_i, now.to_time.to_i, 'all')
+      client.app.crashfree('53b2fb0fe499b52221000147', seven_days_ago.to_time.to_i, now.to_time.to_i, 'all')
     end
 
     def fabric_username
