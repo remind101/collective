1dd9229ced072d6fbb0c427ccb5acd669d9d49fd
Alex Leffelman <alex@remind101.com>
2017-04-04 12:39:25 -0500

    Parameterized Fabric credentials;

--- lib/collective/collectors/fabric.rb
+++ lib/collective/collectors/fabric.rb
@@ -2,6 +2,8 @@ module Collective::Collectors
   class Fabric < Collective::Collector
     requires :fabricio
 
+    resolution '24h'
+
     collect do
       group 'fabric' do |group|
         group.instrument 'ios.crash_free.sessions.7_days', crash_free_7_days
@@ -10,8 +12,8 @@ module Collective::Collectors
 
     def client
       Fabricio::Client.new do |config|
-        config.username = 'alex@remind101.com'
-        config.password = 'Ransom00'
+        config.username = fabric_username
+        config.password = fabric_password
       end
     end
 
@@ -20,6 +22,14 @@ module Collective::Collectors
       seven_days_ago = now - 7
 
       fabric.app.crashfree('53b2fb0fe499b52221000147', seven_days_ago.to_time.to_i, now.to_time.to_i, 'all')
+    end
+
+    def fabric_username
+      options[:username]
+    end
+
+    def fabric_password
+      options[:password]
     end
   end
 end
